d3835aeabc2cdfb838b72d496c69c1522b2fe765
HoDang0103 <94161432+HoDang0103@users.noreply.github.com>
2022-06-08 03:21:18 +0700

    add card

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import {Text, Image, ImageBackground, View, StyleSheet} from 'react-native';
+import Card from './src/component/TinderCard'
+import users from './assets/data/users'
+
+// const jeff={
+//   name: 'Jeff',
+//   bio: 'Im Jeff',
+//   image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg'
+// }
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-        <ImageBackground source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG'}}
-        style={styles.image}
-      >
-        <View style={styles.cardInner}>
-        <Text style={styles.name}>
-          Elon Musk
-        </Text>
-        <Text style={styles.bio}>duihfsdhgfsidohfgdsi hgsodig osoidhgidh d di dihgdi</Text>
-        </View>
-      </ImageBackground>
-    </View>
+      <Card user={users[3]}/>
     </View>
     );
 };
@@ -25,46 +22,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-  },
-  card: {
-    width: '95%',
-    height: '70%',
-    borderRadius: 10,
-
-    shadowColor: "#000",
-    //use for ios
-    // shadowOffset: {
-    //   width: 0,
-    //   height: 5,
-    // },
-    // shadowOpacity: 0.36,
-    // shadowRadius: 6.68,
-
-    elevation: 11,
-  },
-  image:
-  {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-
-    justifyContent: 'flex-end',
-  },
-  name: {
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  bio: {
-    fontSize: 24,
-    color: 'white',
-    lineHeight: 24,
-  },
-  cardInner: {
-    padding: 10,
-  },
-  
+  }
 })
 
 export default App;
---
+++ src/component/TinderCard/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+
+const Card = (props) => {
+    const {name, image, bio} = props.user
+    return(
+        <View style={styles.card}>
+            <ImageBackground source={{uri: image}}
+            style={styles.image}
+            >
+                <View style={styles.cardInner}>
+                <Text style={styles.name}>
+                {name}
+                </Text>
+                <Text style={styles.bio}>{bio}</Text>
+                </View>
+            </ImageBackground>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    pageContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    card: {
+      width: '95%',
+      height: '70%',
+      borderRadius: 10,
+  
+      shadowColor: "#000",
+      //use for ios
+      // shadowOffset: {
+      //   width: 0,
+      //   height: 5,
+      // },
+      // shadowOpacity: 0.36,
+      // shadowRadius: 6.68,
+  
+      elevation: 11,
+    },
+    image:
+    {
+      width: '100%',
+      height: '100%',
+      borderRadius: 10,
+      overflow: 'hidden',
+  
+      justifyContent: 'flex-end',
+    },
+    name: {
+      fontSize: 30,
+      color: 'white',
+      fontWeight: 'bold',
+    },
+    bio: {
+      fontSize: 24,
+      color: 'white',
+      lineHeight: 24,
+    },
+    cardInner: {
+      padding: 10,
+    },
+})
+
+export default Card;
