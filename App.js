d77b00d1a0d97d79fbab9737609f9e32367a49cb
herodang <bestervn@gmail.com>
2022-06-11 10:40:03 +0700

    add button bar

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import 'react-native-gesture-handler';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, SafeAreaView ,Text, Pressable} from 'react-native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+import Fontisto from 'react-native-vector-icons/Fontisto'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
 import MatchSrceen from "./src/Srceens/MatchSrceen";
+import HomeScreen from './src/Srceens/HomeSrceen'
+
 
 
 // const jeff={
@@ -13,20 +20,62 @@ import MatchSrceen from "./src/Srceens/MatchSrceen";
 // }
 
 const App = () => {
+  const [activeScreen, setActiveScreen] = useState('HOME');
+
+  const color = '#b5b5b5';
+  const activeColor= '#F76C6B'
 
   return (
-    <GestureHandlerRootView style={styles.pageContainer}>
-     <MatchSrceen />
-    </GestureHandlerRootView>
+    <SafeAreaView style={styles.root}>
+      <GestureHandlerRootView style={styles.pageContainer}>
+        <View style={styles.topNavigation}>
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Fontisto
+              name="tinder"
+              size={24}
+              color={activeScreen === 'HOME' ? activeColor : color}/>
+          </Pressable>
+          <Pressable onPress={() => setActiveScreen('STAR')}>
+            <MaterialCommunityIcons
+              name="star-four-points"
+              size={24}
+              color={activeScreen === 'STAR' ? activeColor : color}/>
+          </Pressable>
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons
+              name="ios-chatbubbles"
+              size={24}
+              color={activeScreen === 'CHAT' ? activeColor : color}/> 
+          </Pressable>
+          <Pressable onPress={() => setActiveScreen('USER')}>
+            <FontAwesome
+              name="user"
+              size={24}
+              color={activeScreen === 'USER' ? activeColor : color}/>
+          </Pressable>
+        </View>
+          {activeScreen === 'HOME' && <HomeScreen/>}
+          {activeScreen === 'CHAT' && <MatchSrceen/>}
+      </GestureHandlerRootView>
+    </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   pageContainer: {
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
   },
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10
+  }
 });
 
 export default App;
--- src/Srceens/HomeSrceen.js
+++ src/Srceens/HomeSrceen.js
@@ -4,6 +4,10 @@ import Card from '../component/TinderCard'
 import users from '../../assets/data/users.js'
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+import Entypo from 'react-native-vector-icons/Entypo'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+
 
 import AnimatedStack from "../component/AnimatedStack";
 
@@ -31,6 +35,23 @@ const onSwipeRight = (user) => {
      onSwipeLeft={onSwipeLeft}
      onSwipeRight={onSwipeRight}
      />
+     <View style={styles.icons}>
+        <View style={styles.button}>
+          <FontAwesome name="undo" size={24} color="#FBD88B"/>
+        </View>
+        <View style={styles.button}>
+          <Entypo name="cross" size={24} color="#F76C6B"/>
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="star" size={24} color="#3AB4CC"/>
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="heart" size={24} color="#4FCC94"/>
+        </View>
+        <View style={styles.button}>
+          <Ionicons name="flash" size={24} color="#A65CD2"/>
+        </View>
+     </View>
     </GestureHandlerRootView>
     );
 };
@@ -40,8 +61,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-    width: '100%'
+    width: '100%',
+    backgroundColor:'#ededed'
   },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10
+  },
+  button: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50
+  }
 });
 
 export default HomeSrceen;
